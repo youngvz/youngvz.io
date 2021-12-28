006f819c3af2188882d7b907703a8f7fa4099987
Viraj <hello@virajashah.com>
2021-12-28 14:46:57 -0500

    Refactored meta tag implementation

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <html lang="en"/>
+          <link rel="canonical" href="https://youngvz.io/" />
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+
+          <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Vibur&display=swap" rel="stylesheet"/>
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
           <script
--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,11 @@ export default function Home(){
     return (
         <div className={styles.pageWrapper}>
             <Head>
-                <html lang="en"/>
-
                 <meta charSet="utf-8"/>
                 <title>youngvz</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
 
                 <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌊</text></svg>"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com"/>
-
-                <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
-                <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100&display=swap" rel="stylesheet"/>
-                <link href="https://fonts.googleapis.com/css2?family=Vibur&display=swap" rel="stylesheet"/>
-
                 <meta name="title" content="youngvz"/>
                 <meta name="description" content="Vaporwave inspired website."/>
 
